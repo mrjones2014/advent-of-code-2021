73d19c8c7d9d005f08a0814623647edffd85dabe
Mat Jones <mat@mjones.network>
2021-12-03 15:04:18 -0500

    sovle puzzle 2 part 2

--- src/bin/puzzle2.rs
+++ src/bin/puzzle2.rs
@@ -36,8 +36,7 @@ impl Display for Direction {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let input = input_parser::parse("puzzle2");
+fn part_1(input: &Vec<String>) -> Result<(), Box<dyn Error>> {
     let mut coordinates = (0, 0); // horizontal pos, depth
     for movement in input.iter() {
         let direction = Direction::from(movement);
@@ -52,11 +51,52 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
+    println!("Part 1\n=======");
     println!(
         "Coordinates: horizontal: {}, depth: {}",
         coordinates.0, coordinates.1
     );
-    println!("X * Z = {}", coordinates.0 * coordinates.1);
+    println!("horizontal * depth = {}", coordinates.0 * coordinates.1);
+
+    Ok(())
+}
+
+fn part_2(input: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    let mut coordinates = (0, 0, 0); // horizontal pos, depth, aim
+    for movement in input.iter() {
+        let direction = Direction::from(movement);
+        let value: i32 = movement
+            .replace(format!("{}", direction).as_str(), "")
+            .trim()
+            .parse()?;
+        match direction {
+            Direction::FORWARD => {
+                coordinates = (
+                    coordinates.0 + value,
+                    coordinates.1 + (coordinates.2 * value),
+                    coordinates.2,
+                )
+            }
+            Direction::DOWN => coordinates = (coordinates.0, coordinates.1, coordinates.2 + value),
+            Direction::UP => coordinates = (coordinates.0, coordinates.1, coordinates.2 - value),
+        }
+    }
+
+    println!("Part 2\n=======");
+    println!(
+        "Coordinates: horizontal: {}, depth: {}, aim: {}",
+        coordinates.0, coordinates.1, coordinates.2
+    );
+    println!("horizontal * depth = {}", coordinates.0 * coordinates.1);
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = input_parser::parse("puzzle2");
+    part_1(&input)?;
+    println!("");
+    part_2(&input)?;
 
     Ok(())
 }
