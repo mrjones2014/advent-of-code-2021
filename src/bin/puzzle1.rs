2f43f84e4a64f62ce23fe0ed275ee7f2d1f556ee
Mat Jones <mat@mjones.network>
2021-12-03 14:24:08 -0500

    solve puzzle 1

--- src/bin/puzzle1.rs
+++ src/bin/puzzle1.rs
@@ -1,6 +1,14 @@
 use utils::input_parser;
 
 fn main() {
-    let input = input_parser::parse("puzzle1");
-    println!("{}", input.join("\n"))
+    let input: Vec<i32> = input_parser::parse("puzzle1")
+        .into_iter()
+        .map(|line| line.parse().unwrap())
+        .collect();
+    let num_increases = input
+        .windows(2)
+        .filter(|window| window[1] > window[0])
+        .count();
+
+    println!("{}", num_increases);
 }
