7e0547397b4d1b0637f88c692c149fdb92887b72
Mat Jones <mat@mjones.network>
2021-12-07 13:54:52 -0500

    fix bug with puzzle7 not working for the general case

--- src/bin/puzzle7.rs
+++ src/bin/puzzle7.rs
@@ -1,12 +1,11 @@
 use std::error::Error;
-
 use utils::input_parser;
 
-fn compute_min_fuel_movement<F>(inputs: &[i32], mean: &i32, compute_fuel_usage: F) -> i32
+fn compute_min_fuel_movement<F>(inputs: &[i32], compute_fuel_usage: F) -> i32
 where
     F: Fn(i32, i32) -> i32,
 {
-    (*inputs.iter().min().unwrap()..(mean + 1))
+    (*inputs.iter().min().unwrap()..(*inputs.iter().max().unwrap() + 1))
         .into_iter()
         .map(|movement| {
             inputs
@@ -23,14 +22,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         .split(',')
         .map(|s| s.parse().expect("Failed to parse input to i32"))
         .collect::<Vec<i32>>();
-    let mean = inputs.iter().sum::<i32>() / inputs.len() as i32;
     println!(
         "Part 1: {}",
-        compute_min_fuel_movement(&inputs, &mean, |movement, input| (input - movement).abs())
+        compute_min_fuel_movement(&inputs, |movement, input| { (input - movement).abs() })
     );
     println!(
         "Part 2: {}",
-        compute_min_fuel_movement(&inputs, &mean, |movement, input| {
+        compute_min_fuel_movement(&inputs, |movement, input| {
             let n = (input - movement).abs();
             (n * (n + 1)) / 2
         })
